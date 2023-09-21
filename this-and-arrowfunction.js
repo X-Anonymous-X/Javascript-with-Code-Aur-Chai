410e36c5deffbf4f677065e689dc0d3b46ad5c50
x-Anonymous-x <sigma302hassan@gmail.com>
2023-09-21 16:31:03 +0500

    adding more info about this

--- this-and-arrowfunction.js
+++ this-and-arrowfunction.js
@@ -1,7 +1,7 @@
 const obj = {
     name: 'hassan',
-    age:18,
-    message: function(){
+    age: 18,
+    message: function () {
         return `${this.name} is ${this.age} old`
     }
 }
@@ -11,7 +11,7 @@ console.log(obj)
 
 // this method only works in objects
 console.log(this)
-function thisMethod () {
+function thisMethod() {
     console.log(this)
 }
 thisMethod()
@@ -20,43 +20,90 @@ thisMethod()
 // this is normal functions
 
 // const newVar = function (operand1 , operand2) {
-    // return operand1 + operand2
+// return operand1 + operand2
 // }
 // console.log(newVar(20 ,40))
 
 // this is arrow function where we remove "function" keyword and add arrow after the parenthesis as shown
-const newVar = (operand1 , operand2) => {
+const newVar = (operand1, operand2) => {
     return operand1 + operand2
 }
-console.log(newVar(20 ,40))
+console.log(newVar(20, 40))
 // the output of arrow function and normal function is same 
 
 // This is Another method of arrow function known as "Implicit Return Arrow Function" or "single line funtion"
 // we add parenthesis instead of return keyword
 
 const Implicit1 = (val1, val2) => val1 + val2
-console.log(Implicit1(3,9))
+console.log(Implicit1(3, 9))
 
 
 const Implicit = (val1, val2) => (val1 + val2)
-console.log(Implicit(20 , 40))
+console.log(Implicit(20, 40))
 
 // now if want to add object in arrow function
 
 // without parenthesis the return function will not work for object
-const object1 = () => {name : "hassan"}
+const object1 = () => { name: "hassan" }
 
 console.log(object1())
 // using parenthesis
-const object2= () => ({name : "hassan"})
+const object2 = () => ({ name: "hassan" })
 
 console.log(object2())
 
 const newObj = {
     name: 'hassan',
-    age:18,
-    message: function(){
+    age: 18,
+    message: function () {
         return this.name + ' is ' + this.age + ' years old '
     }
 }
 console.log(newObj.message())
+
+// ================================================================
+
+let a = myFunction()
+let b = myFunction()
+
+function myFunction() {
+    return {
+        count : 0,
+        innerFunction: function () {
+            a.count++;
+            b.count++;
+        }
+    }
+}
+a.innerFunction()
+a.innerFunction()
+a.innerFunction()
+a.innerFunction()
+a.innerFunction()
+b.innerFunction()
+b.innerFunction()
+b.innerFunction()
+b.innerFunction()
+console.log(a)
+console.log(b)
+
+// instead of that we use 'this' keyword
+
+let c = myFunction()
+let d = myFunction()
+
+function myFunction() {
+    return {
+        count : 0,
+        innerFunction: function () {
+            this.count++;
+        }
+    }
+}
+
+c.innerFunction()
+c.innerFunction()
+c.innerFunction()
+d.innerFunction()
+d.innerFunction()
+console.log(c,d)
