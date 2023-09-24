f5ef226656059427932345c1a94a9a2b8406e8bc
x-Anonymous-x <sigma302hassan@gmail.com>
2023-09-24 22:02:13 +0500

    switch statement

---
+++ control_flow_4/switch.js
@@ -0,0 +1,38 @@
+// Basic syntax of switch statement
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 'january';
+// switch (month) {
+//     case 1:
+//     case 'january':
+//         console.log('January')
+//         break;
+//     case 2:
+//     case 'february':
+//         console.log('Feburary')
+//         break;
+//     case 3:
+//     case 'march':
+//         console.log('March')
+//         break;
+//     case 4:
+//     case 'april':
+//         console.log('April')
+//         break;
+//     case 5:
+//     case 'may':
+//         console.log('May')
+//         break;
+
+//     default:
+//         console.log('Invalid Month')
+//         break;
+// }
+
