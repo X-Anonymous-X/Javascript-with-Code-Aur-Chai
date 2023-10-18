be31f7bb5ed6f9a27a36bc770b182454b1a5ecb5
x-Anonymous-x <sigma302hassan@gmail.com>
2023-10-18 11:46:32 +0500

    adding async await info in promises

--- 09_advance_one/promises/promises.js
+++ 09_advance_one/promises/promises.js
@@ -7,27 +7,29 @@
 
 // Pending: The promise is not yet completed.
 
-// Fulfilled: The promise has been completed successfully.
+// Fulfilled: The promise has been completed successfully. 
 
 // Rejected: The promise has been completed with an error.
 
+// we use '.then' to handle the (resolved value) and '.catch' to handle the (rejected value)
+
 new Promise(function (resolve, reject) {
-    console.log('Pending')
+    console.log('Processing...')
     setTimeout(function () {
-        let error = true;
+        let error = false;
         if (!error) {
             return resolve('I am fullfilled')
         } else {
             return reject('I am failed')
         }
-    }, 1000)
+    }, 3000)
 })
     .then(function (pass) {
         console.log(pass);
     })
     .catch(function (fail) {
         console.log(fail);
-    })
+    }).finally(() => console.log('Process Complete'))
 
 // Project
 
@@ -74,3 +76,24 @@ btn.addEventListener('mouseover', function () {
     p[0].innerHTML = null
     p[1].innerHTML = null
 })
+
+// Promises with async await
+
+const prom = new Promise((resolve, reject) => {
+    let error = true;
+    if (!error) {
+        resolve('Promise Resolved')
+    }
+    else {
+        reject('Promise Rejected')
+    }
+})
+async function promResponse() {
+    try {
+        const response = await prom // waiting to consume prom response
+        console.log(response)
+    } catch (error) {
+        console.log(error)
+    }
+}
+promResponse()
